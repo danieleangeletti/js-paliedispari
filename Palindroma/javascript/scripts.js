cc95e31ffefb1df1d274261625b5bc9996f7e1c4
danieleangeletti <angelettidaniele@gmail.com>
2023-12-12 14:23:53 +0100

    inizializzazione e primo step (senza function)

---
+++ Palindroma/javascript/scripts.js
@@ -0,0 +1,15 @@
+const parola = "felice";
+let lettere_parola = "";
+let lettere_parola_al_contrario = "";
+
+for (let i = 0; i < parola.length; i++) {
+    lettere_parola = lettere_parola.concat(parola[i]);
+}
+
+console.log(lettere_parola);
+
+for (let i = parola.length - 1; i >= 0; i-- ) {
+    lettere_parola_al_contrario = lettere_parola_al_contrario.concat(parola[i]);
+}
+
+console.log(lettere_parola_al_contrario);
